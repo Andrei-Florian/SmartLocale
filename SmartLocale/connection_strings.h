4978ad94b11381207b15751cc68ebf5fd0d48b41
AndreiFlorian <lightpro64@gmail.com>
2019-12-01 00:33:19 +0000

    updated code

--- SmartLocale/connection_strings.h
+++ SmartLocale/connection_strings.h
@@ -1,4 +1,4 @@
 #pragma once
 
 // Define your connection string here.  The connection string is required to connect to Azure.
-#define MY_CONNECTION_STRING "HostName=universum-azuresphere-hub.azure-devices.net;DeviceId=UniverumAzureSphere;SharedAccessKey=H7rQJ+6AzraZnJhuZ1iYq8bCg9TCxUMouwtmMAeBpyw="
+#define MY_CONNECTION_STRING "" // EDIT
